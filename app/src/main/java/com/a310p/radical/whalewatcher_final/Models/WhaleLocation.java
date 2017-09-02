23291a9bf31d009f1551f4b62868027b025f0252
zhangzeyao <460737263@qq.com>
2017-09-02 14:05:16 +1000

    Initial commit

---
+++ app/src/main/java/com/a310p/radical/whalewatcher_final/Models/WhaleLocation.java
@@ -0,0 +1,8 @@
+package com.a310p.radical.whalewatcher_final.Models;
+
+/**
+ * Created by zhangzeyao on 2/9/17.
+ */
+
+public class WhaleLocation {
+}
--- app/src/main/java/com/a310p/radical/whalewatcher_final/SiteActivity.java
+++ app/src/main/java/com/a310p/radical/whalewatcher_final/SiteActivity.java
@@ -1,101 +1,189 @@
 package com.a310p.radical.whalewatcher_final;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class SiteActivity extends AppCompatActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
 
-    private GoogleMap mgoogleMap;
-    private Location currentLocation;
-    private GoogleApiClient mAPiClient;
+public class SiteActivity extends AppCompatActivity implements OnMapReadyCallback{
 
-    private static final long MAX_UPDATE_INTERVAL = 10000; // 10 seconds
-    private static final long MIN_UPDATE_INTERVAL = 2000; // 2 seconds
-    private static final LatLng LOCATION_CAULFIELD = new LatLng(-37.8770, 145.0443);
+    private GoogleMap map;
+    private CameraPosition cameraPosition;
+
+    private GeoDataClient geoDataClient;
+    private PlaceDetectionClient placeDetectionClient;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private final LatLng defaultLocation = new LatLng(-37.8840,145.0266);
+    private static final int DEFUALT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    private boolean locationPermissionGranted;
+
+    private Location lastKnownLocation;
+
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
 
     @Override
-    protected void onStart(){
-        mAPiClient.connect();;
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop(){
-        mAPiClient.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_site);
-        if(mAPiClient == null){
-            mAPiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+
+        if(savedInstanceState != null){
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
         }
 
-        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapFragment);
+        setContentView(R.layout.activity_site);
+        geoDataClient = Places.getGeoDataClient(this,null);
+        placeDetectionClient = Places.getPlaceDetectionClient(this,null);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        if(map != null){
+            outState.putParcelable(KEY_CAMERA_POSITION, map.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION,lastKnownLocation);
+            super.onSaveInstanceState(outState);
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mgoogleMap = googleMap;
-        mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LOCATION_CAULFIELD,15));
-        AddDefaultMakers();
+        map = googleMap;
+        map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                View inforWindow = getLayoutInflater().inflate(R.layout.custom_info_contents,(FrameLayout)findViewById(R.id.mapFragment));
+
+                TextView title = ((TextView)inforWindow.findViewById(R.id.titleText));
+                title.setText(marker.getTitle());
+
+                TextView snippet = ((TextView)inforWindow.findViewById(R.id.spinnetText));
+                snippet.setText(marker.getSnippet());
+
+                return inforWindow;
+            }
+        });
+
+        getLocationPermission();
+
+        updateLocationUI();
+
+        getDeviceLocation();
+
     }
 
-    private void AddDefaultMakers(){
-        mgoogleMap.addMarker(new MarkerOptions().position(LOCATION_CAULFIELD).title("Monash Caulfield"));
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        currentLocation = location;
-        changeMapLocation();
-    }
-
-    private void changeMapLocation(){
-        if(currentLocation != null && mgoogleMap !=null){
-            LatLng newPosition = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
-            mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPosition,15));
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permission[],@NonNull int[] grantResults){
+        locationPermissionGranted = false;
+        switch (requestCode){
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION : {
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+
+        updateLocationUI();
+    }
+
+    private void getDeviceLocation(){
         try{
-            LocationRequest locationRequest = LocationRequest.create().setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
-                    .setInterval(MAX_UPDATE_INTERVAL).setFastestInterval(MIN_UPDATE_INTERVAL);
-            LocationServices.FusedLocationApi.requestLocationUpdates(mAPiClient,locationRequest,this);
-        }catch (SecurityException secEx){
-            Toast.makeText(this,"ERROR: Please enable location services",Toast.LENGTH_LONG).show();
+            if(locationPermissionGranted){
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if(task.isSuccessful()){
+                            lastKnownLocation = task.getResult();
+                            if(lastKnownLocation==null){
+                                getLocationPermission();
+                            }else{
+                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude()), DEFUALT_ZOOM));
+                            }
+                        } else{
+                            Log.i("Error", "Null location, default will be used");
+                            Log.i("Exception", "Exception:"+task.getException().toString());
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFUALT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+
+        } catch (SecurityException e){
+            Log.e("Exception: %s", e.getMessage());
         }
     }
 
-    @Override
-    public void onConnectionSuspended(int i) {
+    private void updateLocationUI(){
+        if(map == null){
+            return;
+        }
+        try{
+            if(locationPermissionGranted){
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+            }
+            else{
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
 
+            }
+        } catch (SecurityException sexExp){
+            Log.i("Exception","Exception:"+sexExp.getMessage());
+        }
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
-    }
 }
